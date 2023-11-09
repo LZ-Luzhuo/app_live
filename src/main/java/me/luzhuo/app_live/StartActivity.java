d9f3205def785b57bc0c970bd9d21000f2e49800
Luzhuo <LZ-Luzhuo@gmail.com>
2023-11-09 22:09:12 +0800

    update

--- src/main/java/me/luzhuo/app_live/StartActivity.java
+++ src/main/java/me/luzhuo/app_live/StartActivity.java
@@ -14,7 +14,7 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // startActivity(new Intent(this, LiveMainActivity.class)); hello
+        // startActivity(new Intent(this, LiveMainActivity.class)); helloword
         ARouter.getInstance().build(BaseRouterPath.liveMainActivityPath).navigation();
     }
 }
